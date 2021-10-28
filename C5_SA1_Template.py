178397da87cce4ce5a09b3af8a048596cbf52293
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-10-28 16:29:54 +0530

    Update C5_SA1_Template.py

--- C5_SA1_Template.py
+++ C5_SA1_Template.py
@@ -1,11 +1,11 @@
 # Import pygame library
 
-# Initialze pygame
- 
+# Initialzing pygame
+pygame.init() 
 # Create a game screen of width=600 and height=600 using pygame.display.set_mode() and name it as "screen"
-
+screen = pygame.display.set_mode((600, 600))
 # Create a title of game window to "Breakout Game" using pygame.display.set_caption()
-
+pygame.display.set_caption("Breakout Game")
 # Define the RGB color combinations of rectangle objects
 WHITE = (255,255,255)
 DARKBLUE = (36,90,190)
@@ -14,26 +14,26 @@ RED = (255,0,0)
 ORANGE = (255,150,0)
 YELLOW = (255,255,0)
 # Create a paddle rectangle object at x=300, y=500, width=60 and height=10 using pygame.Rect() and name it as "paddle"
-
+paddle = pygame.Rect(300,500,60,10)
 # Create a ball rectangle object at x=200, y=250, width=10 and height=10 using pygame.Rect() and name it as "ball"
-
+ball = pygame.Rect(200,250,10,10)
 # Define a variable "ballx" and assign -1 to it.
-
+ballx = -1
 # Define a variable "bally" and assign -1 to it.
-
+bally = -1
 # Define a variable "paddlex" and assign 2 to it.
-
+paddlex = 2
 # Define a variable "carryOn" and assign a value True to it.
-
+carryOn = True
 # Begin while loop with condition as "carryOn"
 
     # Use the for loop to iterate for each event
-        
-        # Check if user clicked on close or quit on game screen
+    
+        # Checking if user clicked on close or quit on game screen
          
             # Change carryOn to False. This indicates that we are done so we exit this while loop 
             
-    # Fill the screen with darkblue color. 
+    # Filling the screen with darkblue color. 
     
       
     # Check for user input to move the paddle
